9dadf8ba57abd3fa62520f178b7bf987a39d986c
Đình Thọ <tiodev@Tiodev-Mac.local>
2026-03-14 14:04:03 +0700

    update_public

--- src/App.tsx
+++ src/App.tsx
@@ -45,8 +45,8 @@ declare global {
   }
 }
 
-const DEFAULT_MASK_URL = '/src/effect/1. Thumbnail TLS1 Studio - tiktok - AI gg11.jpg';
-const DEFAULT_BGM_URL = '/src/effect/newsmusic.mp3';
+const DEFAULT_MASK_URL = '/public/effect/1. Thumbnail TLS1 Studio - tiktok - AI gg11.jpg';
+const DEFAULT_BGM_URL = '/public/effect/newsmusic.mp3';
 
 export default function App() {
   const [content, setContent] = useState('');
